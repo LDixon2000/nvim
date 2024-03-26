6c11b9ede9770eeda3ca054c4640b25ebb8b766c
Leon <i37892@verisk.com>
2024-03-26 20:59:13 +0000

    Updated to the colemak dh custom keybindings

--- init.lua
+++ init.lua
@@ -1,33 +1,30 @@
---[[
+local modes = { 'n', 'x', 'o' }
 
-  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
-  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
-  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
-  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
-  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
-  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+for _, mode in ipairs(modes) do
+  vim.keymap.set(mode, 'm', 'h')
+  vim.keymap.set(mode, 'M', 'H')
 
+  vim.keymap.set(mode, 'h', 'm')
+  vim.keymap.set(mode, 'H', 'm')
 
-Neovim init file
-Version: 0.41.0 - 2021/11/19
-Maintainer: Brainf+ck
-Website: https://github.com/brainfucksec/neovim-lua
+  -- down
+  vim.keymap.set(mode, 'n', 'j')
+  vim.keymap.set(mode, 'N', 'J')
 
---]]
+  vim.keymap.set(mode, 'j', 'n')
+  vim.keymap.set(mode, 'J', 'N')
 
------------------------------------------------------------
--- Import Lua modules
------------------------------------------------------------
-require('settings')
-require('keymaps')
-require('plugins/packer')
-require('plugins/nvim-tree')
-require('plugins/indent-blankline')
-require('plugins/feline')
-require('plugins/vista')
-require('plugins/nvim-cmp')
-require('plugins/nvim-lspconfig')
-require('plugins/nvim-treesitter')
-require('plugins/alpha-nvim')
-require('plugins/easymotion')
-require('plugins/barbar')
+  -- up
+  vim.keymap.set(mode, 'e', 'k')
+  vim.keymap.set(mode, 'E', 'K')
+
+  vim.keymap.set(mode, 'k', 'e')
+  vim.keymap.set(mode, 'K', 'E')
+
+  -- right
+  vim.keymap.set(mode, 'i', 'l')
+  vim.keymap.set(mode, 'I', 'L')
+
+  vim.keymap.set(mode, 'l', 'i')
+  vim.keymap.set(mode, 'L', 'I')
+end
